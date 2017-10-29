be9d12939b2302f1ede9b66e7b9246d73d0b3e10
No1Statistican <30734236+No1Statistican@users.noreply.github.com>
2017-10-29 17:37:04 -0400

    Add files via upload

--- main.R
+++ main.R
@@ -1,3 +1,32 @@
 #forest's directory
 setwd("C:\\Users\\Owner\\Documents\\MEGA\\University of Maryland\\INST 737\\Project")
-read.csv("")
+full_auto<-read.csv("outlier_values <- boxplot.stats(inputData$pressure_height)$outautos.csv",header=TRUE)
+names(full_auto)
+
+full_auto$dateCrawled<-NULL
+full_auto$ID<-seq.int(nrow(full_auto))
+
+full_auto[full_auto==""]<-NA
+
+full_auto$dateCreated<-NULL
+full_auto$lastSeen<-NULL
+full_auto$nrOfPictures<-NULL
+full_auto$seller<-NULL
+full_auto$abtest<-NULL
+full_auto$offerType<-NULL
+
+#change x later
+#full_auto$pricec[full_auto$price>x or full_auto$price==0]<-NA
+
+full_auto$price[full_auto$price==0]<-NA
+full_auto1<-data.frame()
+full_auto1$price<-na.omit(full_auto$price)
+
+quar<-quantile(full_auto1$price,probs=seq(0.25,0.75,0.5))
+
+quar_df<-as.data.frame(quar)
+
+iqr<-1.5*(max(quar_df$quar)-min(quar_df$quar))
+
+u_limit<-iqr+max(quar_df$quar)
+l_limit<-min(quar_df$quar)-iqr
